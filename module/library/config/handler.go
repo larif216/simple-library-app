91d272a2e2ed661ed210dfb4948a0f039754c889
Muhamad Lutfi Arif <muhamadlutfiarif10@gmail.com>
2025-01-22 13:14:37 +0700

    add cors middleware

--- module/library/config/handler.go
+++ module/library/config/handler.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"net/http"
 	"simple-library-app/module/library/internal/handler"
 
 	"github.com/gorilla/mux"
@@ -12,4 +13,22 @@ func RegisterLibraryHandlers(router *mux.Router, config *LibraryConfig) {
 
 	router.HandleFunc("/api/books", h.ListBook).Methods("GET")
 	router.HandleFunc("/api/pickup-schedule/create", h.CreatePickupSchedule).Methods("POST")
+
+	router.Use(corsMiddleware)
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
